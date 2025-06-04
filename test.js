6817f882ece12bcd478d7e71cd1724e3acc58353
lhotovy <lhotovy@centrum.cz>
2025-06-04 12:32:30 +0200

    Updated tests

--- test.js
+++ test.js
@@ -2,15 +2,16 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 export const options = {
   stages: [
-    { duration: '5s', target: 5 },
+    { duration: '5s', target: 30 },
     { duration: '1m', target: 30 },
-    { duration: '5s', target: 5 },
+    { duration: '5s', target: 0 },
   ],
   thresholds: {
     // 99% of requests must finish within 1000ms.
     http_req_duration: ['p(99) < 1000'],
   },
 };
+
 export default function () {
   const page = http.get('http://test.k6.io');
 
@@ -19,6 +20,5 @@ export default function () {
   });
 
   //console.log(page.body);
-
   sleep(1);
 }
--- test2.js
+++ test2.js
@@ -1,75 +1,49 @@
 import http from 'k6/http';
+import { sleep, check } from 'k6';
 
-import { sleep } from 'k6';
-import { check } from 'k6';
-
-export let options = {
+export let options = { 
   stages: [
-    { duration: '5s', target: 1 },
-    { duration: '30s', target: 3 },
-    { duration: '5s', target: 1 },
+    { duration: '5s', target: 10 },
+    { duration: '25s', target: 30 },
+    { duration: '5s', target: 10 },
   ],
   thresholds: {
     // 99% of requests must finish within 1000ms.
-    http_req_duration: ['p(99) < 1000'],
+    http_req_duration: ['p(99) < 2000'],
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
   },
 };
 
-
 export default function () {
-  let homepage = http.get('https://onlineboutique.dev');
+  let homepage = http.get('https://demowebshop.tricentis.com/desktops');  
 
     check(homepage, {
       'status is 200': (r) => r.status === 200,
     });
 
-  const homePageResponse = homepage.body;
+    const links = [
+      "/build-your-cheap-own-computer", 
+      "/build-your-own-computer", 
+      "/build-your-own-expensive-computer-2",
+      "/desktop-pc-with-cdrw", 
+      "/desktop-pc", 
+      "/simple-computer"
+    ];
 
-  const linkpattern = /<a href="\/product\/(.+?)">/g;
-
-  const matches = homePageResponse.matchAll(linkpattern);
-  const matchesCount =  (homePageResponse.match(linkpattern) || []).length;
   let allProducts = [];
-  for (const match of matches){
-      console.log(match[1]);
-      allProducts.push(match[1]);
+  for (const link of links){
+      allProducts.push(link);
   }
-  console.log(allProducts[1]);
-  const randomLink = Math.floor(Math.random() * matchesCount)+1;
-  console.log("Random Number is: " + randomLink);
-
-  let productLink = "https://onlineboutique.dev" + "/product/" + allProducts[randomLink];
-  let productLinkClick = http.get(productLink);
-
-  console.log(productLinkClick.url);
-
-  check(productLinkClick, {
-    'status is 200': (r) => r.status === 200,
-  });
-
-  // Add to Cart
-  let cartLink = "https://onlineboutique.dev/cart";
-  let cartParams = cartLink + "?product_id=" + allProducts[randomLink] + "&quantity=1";
-
-  let addToCart = http.post(cartParams);
-  console.log(cartParams);
-  console.log(addToCart.url);
-  console.log(addToCart.status);
-
-  //Checkout
-  let checkoutParams = "?email=someone%40example.com&street_address=1600+Amphitheatre+Parkway&zip_code=94043&city=Mountain+View&state=CA&country=United+States&credit_card_number=4432-8015-6152-0454&credit_card_expiration_month=1&credit_card_expiration_year=2022&credit_card_cvv=672";
-  let checkoutLink = "https://onlineboutique.dev/cart/checkout" + checkoutParams;
-
-  let checkout = http.post(checkoutLink);
-
-  console.log(checkout.status);
-  //console.log(checkout.body);
-
-  check(checkout, {
-    'status is 200': (r) => r.status === 200,
-    'Order Complete Check': (r) => r.html('h3').text().includes('Your order is complete!'),
-  });
-
+  
+  // Visit each product page
+  for (const product of allProducts) {
+    let productLink = "https://demowebshop.tricentis.com" + product;
+    let productPage = http.get(productLink);
+    //console.log("Visited: " + productLink + ", Status: " + productPage.status);
+    check(productPage, {
+      'status is 200': (r) => r.status === 200,
+    });
+  }
 
   sleep(1);
 }
